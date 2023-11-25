d3ab1f1408ebd5a4087a3199706b39a30ab5f439
Mark-MDO47 <mdo@ieee.org>
2023-11-25 09:57:29 -0800

    mdo_qualia_paint.py cycles through pix/*.bin images

--- mdo_qualia_paint/mdo_qualia_paint.py
+++ mdo_qualia_paint/mdo_qualia_paint.py
@@ -6,13 +6,18 @@ Simple painting demo that works with on any touch display
 import displayio
 from adafruit_qualia.graphics import Graphics, Displays
 import io
+import os
 
 ############################################################
-# within(x,y, x_bgn, x_end, y_bgn, y_end)
+# within_region(x,y, region)
 #
-# returns True or False
+# INPUTS:
+#    x, y   - coordinates to check
+#    region - indexes to bitmap: (x_bgn, x_end, y_bgn, y_end)
 #
-def within(x,y, region):
+# returns True or False, True if coordinates within region
+#
+def within_region(x,y, region):
     x_bgn, x_end, y_bgn, y_end = region
     my_rtn = True
     if (x < x_bgn) or (x >= x_end):
@@ -20,10 +25,15 @@ def within(x,y, region):
     if (y < y_bgn) or (y >= y_end):
         my_rtn = False
     return my_rtn
-    # end within()
+    # end within_region()
 
 ############################################################
-# img_565 = color_region(bitmap, color, region)
+# color_region(bitmap, color, region)
+#
+# INPUTS:
+#    bitmap - RGB 565 that refreshes into the screen; width-first then height
+#    color  - RGB 565 color to use
+#    region - indexes to bitmap: (x_bgn, x_end, y_bgn, y_end)
 #
 def color_region(bitmap, color, region):
     x_bgn, x_end, y_bgn, y_end = region
@@ -33,35 +43,17 @@ def color_region(bitmap, color, region):
     # end color_region()
 
 ############################################################
-# img_565 = refresh_right_screen(bitmap, img_565, palette_width, wd, ht, hotspot)
-#
-# left half of screen is first palette_width pixels
-# total screen is wd width by ht height pixels
-# img_565 is the matched set of pixels, arranged width-first
-# bitmap is the screen pixel area
-#
-def refresh_right_screen(bitmap, img_565, palette_width, wd, ht, hot_spot):
-    for i in range(palette_width,wd):
-        for j in range(ht):
-            """
-            # debug checking code
-            if (i < palette_width) or (i > wd):
-                raise RuntimeError("i(%d) out of range." % i)
-            if (j < 0) or (j > ht):
-                raise RuntimeError("j(%d) out of range." % j)
-            if ((i - palette_width + j*(wd-palette_width)) < 0) or ((i - palette_width + j*(wd-palette_width)) >= numPxls):
-                raise RuntimeError("i(%d) j(%d) calc(%d) out of range." % (i, j, i + j*(wd-palette_width)))
-            """
-            bitmap[i, j] = img_565[i - palette_width + j*(wd-palette_width)]
-    color_region(bitmap, 0, hot_spot)
-    # end refresh_right_screen()
-
-
-############################################################
 # img_565 = rd_dotbin_file(fname, numPxls)
 #
 # THIS IS FOR BIG-ENDIAN BINARY FILE 
 #
+# fname   - path to *.bin file created by mdo_tablegen.py
+#              arranged width-first then height
+#              RGB 565 in big-endian format, two bytes per pixel
+# numPxls - total number of pixels expected in *.bin file
+#
+# returns  img_565 - RGB 565 pixels, arranged per .bin file which is width-first
+
 def rd_dotbin_file(fname, numPxls):
     img_565 = [0]*numPxls # make sure we have room
     fptr = io.open(fname,'rb')
@@ -78,6 +70,50 @@ def rd_dotbin_file(fname, numPxls):
 
     return img_565
     # end rd_dotbin_file()
+
+############################################################
+# refresh_right_screen(bitmap, list_of_bin, palette_width, wd, ht, hotspot)
+#
+# INPUTS:
+#    bitmap        - RGB 565 that refreshes into the screen; width-first then height
+#    list_of_bin   - list of paths to *.bin files to display
+#    palette_width - width of the color palette on the left, in pixels
+#    wd, ht        - total width and height of the screen bitmap area
+#    hotspot       - control that cuases another call to this routine
+#
+# INFO:
+#    left half of screen is first palette_width pixels
+#    right have of screen is from coordinate (wd-palette_width) to wd
+#    total screen is wd width by ht height pixels
+#    img_565 is the matched set of pixels, arranged width-first
+#
+# GLOBAL:
+#    which_bin contains index within list_of_bin to use for display
+#
+which_bin = 0 # index of which bin file to use next
+def refresh_right_screen(bitmap, list_of_bin, palette_width, wd, ht, hot_spot):
+    global which_bin
+    # get img_565 and prepare which_bin for next call
+    img_565 = rd_dotbin_file(list_of_bin[which_bin], (wd-palette_width)*ht)
+    which_bin += 1
+    if which_bin >= len(list_of_bin):
+        which_bin = 0
+    # copy img_565 into left half of bitmap 
+    for i in range(palette_width,wd):
+        for j in range(ht):
+            """
+            # debug checking code
+            if (i < palette_width) or (i > wd):
+                raise RuntimeError("i(%d) out of range." % i)
+            if (j < 0) or (j > ht):
+                raise RuntimeError("j(%d) out of range." % j)
+            if ((i - palette_width + j*(wd-palette_width)) < 0) or ((i - palette_width + j*(wd-palette_width)) >= numPxls):
+                raise RuntimeError("i(%d) j(%d) calc(%d) out of range." % (i, j, i + j*(wd-palette_width)))
+            """
+            bitmap[i, j] = img_565[i - palette_width + j*(wd-palette_width)]
+    # color the hot_spot black
+    color_region(bitmap, 0, hot_spot)
+    # end refresh_right_screen()
 
 """
 # %%%%%%%%%%% THIS IS FOR C-LANGUAGE *.H FILE %%%%%%%%%%%
@@ -134,12 +170,17 @@ hot_spot_y_bgn = graphics.display.height - palette_height
 hot_spot_y_end = graphics.display.height
 hot_spot = (hot_spot_x_bgn, hot_spot_x_end, hot_spot_y_bgn, hot_spot_y_end)
 
-# read my image map
+# prepare to read my image map
 numPxls = (graphics.display.width - palette_width) * graphics.display.height # (480 - 160) * 480 - make sure we have room
 
-# img_565 = rd_doth_file("mdo_goggle_565_lc160_320x480.h", numPxls) # much slower
-img_565 = rd_dotbin_file("mdo_goggle_565_lc160_320x480.bin", numPxls)
-
+"""
+img_565 = rd_dotbin_file("pix/mdo_goggle_565_lc160_320x480.bin", numPxls)
+"""
+pix_files = os.listdir("pix")
+list_of_bin = []
+for a_fn in pix_files:
+   if (len(a_fn) > 4) and ((a_fn.rfind(".bin") + len(".bin")) == len(a_fn)):
+       list_of_bin.append("pix" + os.sep + a_fn)
 
 bitmap = displayio.Bitmap(graphics.display.width, graphics.display.height, 65535)
 
@@ -157,8 +198,8 @@ graphics.display.root_group = graphics.splash
 
 current_color = displayio.ColorConverter().convert(0xFFFFFF)
 
-# display my image map
-refresh_right_screen(bitmap, img_565, palette_width, graphics.display.width, graphics.display.height, hot_spot)
+# display next image map
+refresh_right_screen(bitmap, list_of_bin, palette_width, graphics.display.width, graphics.display.height, hot_spot)
 
 for i in range(palette_width):
     color_index = i * 255 // palette_width
@@ -193,14 +234,13 @@ while True:
                 if x < palette_width:
                     current_color = bitmap[x, y]
                     continue
-                if within(x,y, hot_spot):
-                    refresh_right_screen(bitmap, img_565, palette_width, graphics.display.width, graphics.display.height, hot_spot)
+                if within_region(x,y, hot_spot):
+                    refresh_right_screen(bitmap, list_of_bin, palette_width, graphics.display.width, graphics.display.height, hot_spot)
                 else:
                     for i in range(pixel_size):
                         for j in range(pixel_size):
                             x_pixel = x - (pixel_size // 2) + i
                             y_pixel = y - (pixel_size // 2) + j
-
                             if (
                                 0 <= x_pixel < graphics.display.width
                                 and 0 <= y_pixel < graphics.display.height
